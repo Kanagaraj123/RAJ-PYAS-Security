4f9b0899590f3c55c2d3ee05aa094687bddf2cde
Kanagaraj G <161796828+Kanagaraj123@users.noreply.github.com>
2024-03-08 14:54:24 +0530

    Delete PYAS_Extension.py

--- PYAS_Extension.py
+++
@@ -1,8 +0,0 @@
-slist = [".exe",".dll",".com",".msi",".scr",
-         ".js",".bat",".cmd",".ps1",".vbs"]
-
-alist = [".ppt",".pptx",".wps",".txt",".rtf",
-         ".pdf",".xls",".xlsx",".doc",".docx",
-         ".jpg",".jpeg",".png",".webp",".gif",
-         ".mp3",".wav",".aac",".ogg",".flac",
-         ".mp4",".avi",".mov",".wmv",".mkv"]
